bf7f680aa54a327f1ac248b46b048e77d0a896f6
Francisco Vasquez <fjvasquez8@misena.edu.co>
2023-11-16 21:07:45 -0500

    Avance
    Avance

--- src/config/global.js
+++ src/config/global.js
@@ -107,7 +107,7 @@ export default {
       {
         icono: 'fas fa-download',
         titulo: 'Descargar PDF',
-        download: 'downloads/material.zip',
+        download: 'downloads/CFA_01_331316.zip',
       },
       {
         icono: 'fas fa-download',
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,6 +12,9 @@
     p Bienvenidos a este componente formativo relacionado con los fundamentos y la normatividad vigente en la prestación de servicios de salud. Este es la base normativa en el orden la prestación de servicios de salud, en el marco de la calidad que obliga a todos los actores partícipes del proceso. 
     p.mb-5 Se invita a ver el siguiente video en donde encontrará de manera general la información concerniente al tema de la normatividad que regula los procesos de gestión en seguridad sanitaria y salud ambiental:
 
+    figure
+      .video
+        iframe(width="560" height="315" src="https://www.youtube.com/embed/5Ryr4rJn3AQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
     
     
 </template>
